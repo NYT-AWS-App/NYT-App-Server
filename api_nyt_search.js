88db85c96d6fddce89c9073ea85ebef258053768
Griffin Minster <griffinminster@gmail.com>
2023-12-04 21:23:16 -0600

    Formatting fixes

--- api_nyt_search.js
+++ api_nyt_search.js
@@ -6,76 +6,90 @@
 //
 
 exports.nyt_search = async (req, res) => {
-
   console.log("call to /nyt_search...");
 
-  const mySecret = process.env['API Key'];
+  const mySecret = process.env["API Key"];
 
   var search_filter = req.query.search_filter;
   var search_term = req.query.search_term;
 
   try {
-
     // KEYWORD
     if (search_filter === "keyword") {
       try {
         // Search by query param
-        const response = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search_term}&api-key=${mySecret}`);
+        const response = await fetch(
+          `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search_term}&api-key=${mySecret}`
+        );
         const nyt_data = await response.json();
+
         if (response.status === "ERROR") {
-          res.status(400).json({"message": "ERROR", "data": []});
+          res.status(400).json({ message: "ERROR", data: [] });
         }
-        res.json({"message": "success", "data": nyt_data});
+
+        res.json({ message: "success", data: nyt_data });
       } catch (error) {
-        res.status(400).json({"message": error, "data": []});
+        res.status(400).json({ message: error, data: [] });
       }
-    // HEADLINE
+
+      // HEADLINE
     } else if (search_filter === "headline") {
       try {
         // Search by filter query (headline)
-        const response = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=headline:("${search_term}")&api-key=${mySecret}`);
+        const response = await fetch(
+          `https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=headline:("${search_term}")&api-key=${mySecret}`
+        );
         const nyt_data = await response.json();
+
         if (response.status === "ERROR") {
-          res.status(400).json({"message": "ERROR", "data": []});
+          res.status(400).json({ message: "ERROR", data: [] });
         }
-        res.json({"message": "success", "data": nyt_data});
+
+        res.json({ message: "success", data: nyt_data });
       } catch (error) {
-        res.status(400).json({"message": error, "data": []});
+        res.status(400).json({ message: error, data: [] });
       }
-    // AUTHOR
+
+      // AUTHOR
     } else if (search_filter === "author") {
       try {
         // Search by filter query (byline)
-        const response = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=byline:("${search_term}")&api-key=${mySecret}`);
+        const response = await fetch(
+          `https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=byline:("${search_term}")&api-key=${mySecret}`
+        );
         const nyt_data = await response.json();
+
         if (response.status === "ERROR") {
-          res.status(400).json({"message": "ERROR", "data": []});
+          res.status(400).json({ message: "ERROR", data: [] });
         }
-        res.json({"message": "success", "data": nyt_data});
+
+        res.json({ message: "success", data: nyt_data });
       } catch (error) {
-        res.status(400).json({"message": error, "data": []});
+        res.status(400).json({ message: error, data: [] });
       }
-    // DATE (YYYY-MM-DD)
+
+      // DATE (YYYY-MM-DD)
     } else if (search_filter === "date") {
       try {
         // Search by filter query (pub_date)
-        const response = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=pub_date:("${search_term}")&api-key=${mySecret}`);
+        const response = await fetch(
+          `https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=pub_date:("${search_term}")&api-key=${mySecret}`
+        );
         const nyt_data = await response.json();
+
         if (response.status === "ERROR") {
-          res.status(400).json({"message": "ERROR", "data": []});
+          res.status(400).json({ message: "ERROR", data: [] });
         }
-        res.json({"message": "success", "data": nyt_data});
+
+        res.json({ message: "success", data: nyt_data });
       } catch (error) {
-        res.status(400).json({"message": error, "data": []});
+        res.status(400).json({ message: error, data: [] });
       }
     }
-    
-  }
-  catch (err) {
+  } catch (err) {
     res.status(400).json({
-      "message": err.message,
-      "data": []
+      message: err.message,
+      data: [],
     });
   }
-
-}
+};
