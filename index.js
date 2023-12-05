098b61bef1ea7d863249974d751fcc2943052fd4
Griffin Minster <griffinminster@gmail.com>
2023-12-04 21:25:14 -0600

    All endpoints included

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //
-// Express js (and node.js) web service that interacts with 
-// AWS S3 and RDS to provide clients data for building a 
+// Express js (and node.js) web service that interacts with
+// AWS S3 and RDS to provide clients data for building a
 // simple photo application for photo storage and viewing.
 //
 //
@@ -11,11 +11,11 @@
 //  CS 310
 //
 // References:
-// Node.js: 
+// Node.js:
 //   https://nodejs.org/
-// Express: 
+// Express:
 //   https://expressjs.com/
-// MySQL: 
+// MySQL:
 //   https://expressjs.com/en/guide/database-integration.html#mysql
 //   https://github.com/mysqljs/mysql
 // AWS SDK with JS:
@@ -25,33 +25,46 @@
 //   https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/javascript_s3_code_examples.html
 //
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const config = require('./config.js');
+const config = require("./config.js");
 
-const dbConnection = require('./database.js')
+const dbConnection = require("./database.js");
 
-app.use(express.json({strict: false, limit: "50mb"}));
+app.use(express.json({ strict: false, limit: "50mb" }));
 
 app.listen(config.service_port, () => {
   startTime = Date.now();
-  console.log('web service running...');
-  process.env.AWS_SHARED_CREDENTIALS_FILE = config.photoapp_config;
+  console.log("web service running...");
+  process.env.AWS_SHARED_CREDENTIALS_FILE = config.config_file;
 });
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   var uptime = Math.round((Date.now() - startTime) / 1000);
   res.json({
-    "status": "running",
+    status: "running",
     "uptime-in-secs": uptime,
-    "dbConnection": dbConnection.state
+    dbConnection: dbConnection.state,
   });
 });
 
 //
 // service functions:
 //
-var users = require('./api_users.js');
+var add_user = require("./api_user.js");
+var nyt_search = require("./api_nyt_search.js");
+var save = require("./api_save.js");
+var search = require("./api_search.js");
+var articles = require("./api_articles.js");
+var download = require("./api_download.js");
+var delete_article = require("./api_delete.js");
+var get_users = require("./api_users.js");
 
-// //app.get('/stats', (req, res) => {...});
-app.get('/users', users.get_users);  
+app.put("/add_user", add_user.put_user);
+app.get("/nyt_search", nyt_search.nyt_search);
+app.put("/save", save.put_save);
+app.get("/search", search.article_search);
+app.get("/articles", articles.get_articles);
+app.get("/download/:articleid", download.get_download);
+app.delete("/delete", delete_article.delete_article);
+app.get("/users", get_users.get_users);
