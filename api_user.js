a4a6334917d13be43b8684b7df83ec008556c4bb
Griffin Minster <griffinminster@gmail.com>
2023-12-04 21:24:22 -0600

    Formatting fixes

--- api_user.js
+++ api_user.js
@@ -3,28 +3,56 @@
 //
 // Inserts a new user into the database
 //
-const dbConnection = require('./database.js')
+const dbConnection = require("./database.js");
 
 exports.put_user = async (req, res) => {
-
   console.log("call to /user...");
 
   try {
+    var data = req.body; // data => JS object
 
-    var data = req.body;  // data => JS object
-    
-    console.log("/user: inserting new user into RDS...");
-    var insert_sql = `INSERT INTO users (username, pwdhash, bucketfolder) VALUES ("?","?","?")`
-    dbConnection.query(insert_sql, [data.username, data.pwdhash, data.bucketfolder], function (
-  
-    }//try
-    catch (err) {
-      console.log("**ERROR:", err.message);
-  
-      res.status(400).json({
-        "message": err.message,
-        "userid": -1
-      });
-    }//catch
+    //CHECK IF USERNAME IS TAKEN
+    sql_user_query = `SELECT * FROM users WHERE username = ?`;
+    dbConnection.query(sql_user_query, [data.username], function (err, result) {
+      if (err) {
+        res.status(400).json({
+          message: err.message,
+        });
+        return;
+      }
+      if (result.length != 0) {
+        res.status(400).json({
+          message: "Username already taken! Try again.",
+        });
+      }
 
-}//put
+      console.log("/user: inserting new user into RDS...");
+      //USERNAME IS NOT TAKE AND PROCEED WITH INSERTING USER
+      var insert_sql = `INSERT INTO users (username, pwdhash, bucketfolder) VALUES (?,?,?)`;
+      dbConnection.query(
+        insert_sql,
+        [data.username, data.pwdhash, data.bucketfolder],
+        function (err, result) {
+          if (err) {
+            res.status(400).json({
+              message: err.message,
+            });
+            return;
+          }
+          res.json({
+            message: "New user created",
+            userid: result.insertId,
+          });
+        }
+      );
+    });
+  } catch (err) {
+    //try
+    console.log("**ERROR:", err.message);
+
+    res.status(400).json({
+      message: err.message,
+      userid: -1,
+    });
+  } //catch
+}; //put
