83d39fe11c86d6459ae14135272d3d79def7c26a
Griffin Minster <griffinminster@gmail.com>
2023-12-04 21:24:37 -0600

    Initial get users

---
+++ api_users.js
@@ -0,0 +1,33 @@
+//
+// app.get('/users', async (req, res) => {...});
+//
+// Retrieves all user's information
+//
+const dbConnection = require("./database.js");
+
+exports.get_users = async (req, res) => {
+  console.log("call to /users...");
+
+  try {
+    sql = "SELECT * FROM users ORDER BY userid ASC";
+    dbConnection.query(sql, function (err, results) {
+      if (err) {
+        res.status(400).json({
+          message: err.message,
+        });
+        return;
+      } else {
+        res.json({
+          message: "success",
+          users: results,
+        });
+        return;
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+    return;
+  }
+};
